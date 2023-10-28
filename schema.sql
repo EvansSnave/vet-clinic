c208189fdb65451a0e0187fad752b1d844c5dff1
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-10-28 12:07:13 -0500

    finish fourth milestone

--- schema.sql
+++ schema.sql
@@ -9,24 +9,58 @@ CREATE TABLE animals (
     weight_kg decimal(5, 2)
 );
 
-ALTER TABLE animals ADD species char(20);
 
-CREATE TABLE owners (
+ALTER TABLE
+    animals
+ADD
+    species char(20);
+
+
+CREATE TABLE owners(
     id SERIAL PRIMARY KEY,
     full_name varchar(25) DEFAULT 'Unknown',
     age int
 );
 
-CREATE TABLE species (
+CREATE TABLE species(
     id SERIAL PRIMARY KEY,
     name varchar(20)
 );
 
-ALTER TABLE animals ADD PRIMARY KEY (id);
-ALTER TABLE animals DROP COLUMN species;
-ALTER TABLE animals ADD species_id int;
-ALTER TABLE animals ADD CONSTRAINT key_animal_species 
-FOREIGN KEY (species_id) REFERENCES species(id);
-ALTER TABLE animals ADD owners_id int;
-ALTER TABLE animals ADD CONSTRAINT key_animals_owners
-FOREIGN KEY (owners_id) REFERENCES owners(id);
+ALTER TABLE animals
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE animals
+    DROP COLUMN species;
+
+ALTER TABLE animals
+    ADD species_id int;
+
+ALTER TABLE animals
+    ADD CONSTRAINT fk_animals_species
+    FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+    ADD owners_id int;
+
+ALTER TABLE animals
+    ADD CONSTRAINT fk_animals_owners
+    FOREIGN KEY (owners_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name varchar(100),
+	age smallint,
+    date_of_graduation date
+);
+
+CREATE TABLE specializations (
+	species_id int,
+	vets_id int
+);
+
+CREATE TABLE visits (
+	animals_id int,
+	vets_id int,
+	visit_date date
+);
