5b69dd00bc6f1b55ab266bec581c906ca01908e5
Maximo Ortelli <118080546+maximoortelli@users.noreply.github.com>
2023-10-30 19:55:05 -0300

    Delete error of queries.sql
    Delete error of queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,3 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name = 'Luna';
-EXPLAIN ANALYZE SELECT COUNT(*) FROM visits WHERE animals_id = 4;
-EXPLAIN ANALYZE SELECT * FROM visits WHERE vets_id = 2;
-EXPLAIN ANALYZE SELECT * FROM owners WHERE email = 'owner_18327@mail.com';
