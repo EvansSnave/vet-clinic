7daeebf4bae87e5f7e40888f7860ccf5ce727738
Maximo Ortelli <118080546+maximoortelli@users.noreply.github.com>
2023-10-30 19:51:38 -0300

    Update queries.sql
    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name = 'Luna';
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits WHERE animals_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits WHERE vets_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners WHERE email = 'owner_18327@mail.com';
